2ecec8b4c0e6b6c5e75831354d1add074b747786
Amy <amyl6086@gmail.com>
2022-03-16 00:44:15 +0000

    add details link

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -3,13 +3,14 @@ import React from 'react';
 export const Cocktail = ({ idDrink, strDrinkThumb, strDrink, strAlcoholic }) => {
     return (
         <article className="cocktail">
-            <a href={`/cocktail/${idDrink}`}>
-                <img src={strDrinkThumb} alt={strDrink} />
+            <img src={strDrinkThumb} alt={strDrink} />
+            <div className="flex">
                 <div className="cocktail-info">
                     <h3 className="cocktail-name">{strDrink}</h3>
                     <p className="light">{strAlcoholic}</p>
+                    <a href={`/cocktail/${idDrink}`}>Details</a>
                 </div>
-            </a>
+            </div>
         </article>
     );
 };
