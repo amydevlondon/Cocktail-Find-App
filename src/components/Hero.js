8f20667a7984e3b12945afe4f156158ec5429cba
Amy <amyl6086@gmail.com>
2022-03-21 18:41:17 +0000

    delete search link

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,16 +11,10 @@ export const Hero = () => {
             <div className="overlay" />
             <div className="hero-text">
                 <h1>cocktail<FaCocktail className="cocktail-icon" />find</h1>
-                <div className="hero-links">
                     <Link to="/explore">
                         explore cocktails
                         <FiArrowRightCircle className="arrow-icon" />
                     </Link>
-                    <Link to="/search">
-                        search cocktails
-                        <FiArrowRightCircle className="arrow-icon" />
-                    </Link>
-                </div>
             </div>
         </div>
     );
