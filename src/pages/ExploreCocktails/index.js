dbac10296ca29833b261716215c8155e9302ffed
Amy <amyl6086@gmail.com>
2022-03-25 16:51:34 +0000

    add title

--- src/pages/ExploreCocktails/index.js
+++ src/pages/ExploreCocktails/index.js
@@ -7,7 +7,7 @@ import CocktailList from '../../components/CocktailList';
 
 export const ExploreCocktails = () => {
 
-    const [category, setCategory] = useState("randomselection");
+    const [category, setCategory] = useState("popular");
 
     const { isLoading, isError, cocktails } = useFetch(
         `${process.env.REACT_APP_URL}${process.env.REACT_APP_API_KEY}/${category}.php`
@@ -16,16 +16,19 @@ export const ExploreCocktails = () => {
     if (isLoading)
         return <Loading />;
     if (isError)
-        return <Error message={"Something went wrong"}/>;
+        return <Error message={"Something went wrong"} />;
     return (
         <>
-            <form className="form">
-                <select value={category} onChange={(e) => { setCategory(e.target.value); }}>
-                    <option value="popular">Popular</option>
-                    <option value="recent">Recently Added</option>
-                    <option value="randomselection">Random</option>
-                </select>
-            </form>
+        <div className="flex-form">
+                <h3 className="title">Explore</h3>
+                <form className="form">
+                    <select value={category} onChange={(e) => { setCategory(e.target.value); }}>
+                        <option value="popular">Popular</option>
+                        <option value="recent">Recently Added</option>
+                        <option value="randomselection">Random</option>
+                    </select>
+                </form>
+        </div>
             <CocktailList cocktails={cocktails} />
         </>
     );
