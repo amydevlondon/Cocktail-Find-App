033689b1643d9e08623b33937cc38136b19d54fd
Amy <amyl6086@gmail.com>
2022-03-25 16:52:36 +0000

    remove unused flex class div

--- src/components/Cocktail/index.js
+++ src/components/Cocktail/index.js
@@ -6,13 +6,11 @@ export const Cocktail = ({ idDrink, strDrinkThumb, strDrink, strAlcoholic }) => 
     return (
         <article className="cocktail">
             <img src={strDrinkThumb} alt={strDrink} />
-            <div className="flex">
                 <div className="cocktail-info">
                     <h3 className="cocktail-name">{strDrink}</h3>
                     <p className="light">{strAlcoholic}</p>
                     <Link to ={`/cocktail/${idDrink}`}>Details</Link>
                 </div>
-            </div>
         </article>
     );
 };
